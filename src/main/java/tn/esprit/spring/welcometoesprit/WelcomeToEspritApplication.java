67130f7937177bf29a6161adfcf5543fb69bd99e
CoulibalySourouga <Sourouga.Coulibaly@esprit.tn>
2023-01-24 17:26:11 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/spring/welcometoesprit/WelcomeToEspritApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.welcometoesprit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeToEspritApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelcomeToEspritApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/spring/welcometoesprit/WelcomeToEspritApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.welcometoesprit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeToEspritApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
